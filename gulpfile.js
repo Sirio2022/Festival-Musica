cc1f999e38ce941aa97272b349ee046bb8d271d2
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-09-29 15:50:16 -0500

    A full!
    Gulp y Webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
+// CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+// Imagenes
+const webp = require('gulp-webp');
+
 
 function css(done) {
   // Identify the source file SASS
@@ -16,10 +21,25 @@ function css(done) {
   done(); // Signal completion
 }
 
+function versionWebp(done) {
+
+  const versionWebp = {
+    quality: 50
+  }
+
+
+  src('src/img/**/*.{png,jpg}')
+    .pipe(webp( versionWebp))
+    .pipe(dest('build/img'));
+
+  done();
+}
+
 function dev(done) {
   watch('src/scss/**/*.scss', css);
   done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(dev, versionWebp);
