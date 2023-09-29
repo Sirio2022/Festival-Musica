5be49025d602b5a8d1f16006815999b766a0c055
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-09-29 16:24:43 -0500

    A full Gulp
    Todos los formatos modernos de imagenes.

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,16 @@ const plumber = require('gulp-plumber');
 
 // Imagenes
 const webp = require('gulp-webp');
-
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+const avif = require('gulp-avif');
 
 function css(done) {
   // Identify the source file SASS
   src('src/scss/**/*.scss')
     // Process the file
-    .pipe(plumber())
     .pipe(sass())
+    .pipe(plumber())
 
     // Write the destination file
 
@@ -22,14 +24,33 @@ function css(done) {
 }
 
 function versionWebp(done) {
-
   const versionWebp = {
-    quality: 50
-  }
+    quality: 50,
+  };
 
+  src('src/img/**/*.{png,jpg}').pipe(webp(versionWebp)).pipe(dest('build/img'));
+
+  done();
+}
+
+function versionAvif(done) {
+  const versionAvif = {
+    quality: 50,
+  };
+
+  src('src/img/**/*.{png,jpg}').pipe(avif(versionAvif)).pipe(dest('build/img'));
+
+  done();
+}
+
+function minifyImage(done) {
+  const minifyImage = {
+    optimizationLevel: 3,
+  };
 
   src('src/img/**/*.{png,jpg}')
-    .pipe(webp( versionWebp))
+    .pipe(cache(imagemin(minifyImage)))
+    .pipe(imagemin())
     .pipe(dest('build/img'));
 
   done();
@@ -40,6 +61,7 @@ function dev(done) {
   done();
 }
 
-exports.css = css;
+exports.versionAvif = versionAvif;
+exports.minifyImage = minifyImage;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(dev, versionWebp);
+exports.dev = parallel(dev, versionWebp, minifyImage, versionAvif);
